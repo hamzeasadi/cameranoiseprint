0ea8830da3223d156dc2709217b02807fcd12fdd
hamzeasadi <hamzeasadi1@gmail.com>
2023-03-07 17:28:13 +0000

    new disc function

--- model.py
+++ model.py
@@ -52,15 +52,13 @@ class Disc(nn.Module):
     def __init__(self, inch) -> None:
         super().__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(in_channels=inch, out_channels=32, kernel_size=3, stride=2, padding=1), nn.LeakyReLU(negative_slope=0.2), nn.Dropout(0.2),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2), nn.LeakyReLU(negative_slope=0.2), nn.Dropout(0.25), nn.BatchNorm2d(64, momentum=0.8),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2), nn.LeakyReLU(negative_slope=0.2), nn.Dropout(0.25), nn.BatchNorm2d(128, momentum=0.8),
-            nn.Conv2d(in_channels=128, out_channels=256 , kernel_size=3, stride=2), nn.LeakyReLU(negative_slope=0.2), nn.Dropout(0.25), nn.BatchNorm2d(256, momentum=0.8),
-            # nn.Conv2d(in_channels=256, out_channels=512 , kernel_size=3, stride=1)
-            nn.AvgPool2d(kernel_size=3),
+            nn.Conv2d(in_channels=inch, out_channels=16, kernel_size=3, stride=2, padding=1), nn.LeakyReLU(negative_slope=0.2), nn.Dropout(0.2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=2, padding=1), nn.LeakyReLU(negative_slope=0.2), nn.Dropout(0.2), nn.BatchNorm2d(32, momentum=0.8),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1), nn.LeakyReLU(negative_slope=0.2), nn.Dropout(0.2), nn.BatchNorm2d(64, momentum=0.8),
+            nn.Conv2d(in_channels=64, out_channels=128 , kernel_size=3, stride=2, padding=1), nn.LeakyReLU(negative_slope=0.2), nn.Dropout(0.2), nn.BatchNorm2d(128, momentum=0.8),
+
             nn.Flatten(),
-            nn.Linear(in_features=256, out_features=1)
-            
+            nn.Linear(in_features=4*4*128, out_features=1)
         )
 
     def forward(self, x):
