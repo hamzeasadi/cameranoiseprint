1b172a19ef9463b369631b9c2ca48001a5ccfe24
hamzeasadi <hamzeasadi1@gmail.com>
2023-12-10 19:20:03 +0000

    fix

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from torch import nn
 from torch.optim import Adam
 
 from Utils.gutils import Paths
-from Dataset.dataset import create_loader
+from Dataset.dataset import create_loader, Noiseprint_Dataset
 from Model.noiseprint_model import Noise_Print
 from Loss.lossfunction import Loss_Function
 from Engine.engine import Engine
-from Loss.lossfunction import ExamplePairMiner
+
 
 
 
@@ -25,44 +25,55 @@ def main():
     """
 
     paths = Paths()
+    # region
 
-    x = torch.tensor([
-                    [1, 2, 3],
-                    [2, 1, 3],
-                    [2, 2, 2],
-                    [4, 5, 6],
-                    [4, 5, 5],
-                    [1, 3, 3]
-                ], dtype=torch.float32)
-    y = torch.tensor([1,0,1,0,1,0], dtype=torch.long)
-    num_label = y.squeeze().size()
-    print(x.shape)
-    print(y.shape)
+    # x = torch.tensor([
+    #                 [1, 2, 3],
+    #                 [2, 1, 3],
+    #                 [2, 2, 2],
+    #                 [4, 5, 6],
+    #                 [4, 5, 5],
+    #                 [1, 3, 3]
+    #             ], dtype=torch.float32)
+    # y = torch.tensor([1,0,1,0,1,0], dtype=torch.long)
+    # num_label = y.squeeze().size()
+    # print(x.shape)
+    # print(y.shape)
 
 
-    dist = torch.cdist(x1=x, x2=x, p=2)
+    # dist = torch.cdist(x1=x, x2=x, p=2)
     
-    n = num_label[0]
-    dist = dist.flatten()[1:].view(n-1, n+1)[:,:-1].reshape(n, n-1)
-    print(dist)
-    soft_dist = torch.softmax(-dist, dim=1)
-    print(soft_dist)
-    for i in range(n):
-        lbl = y[i]
-        indices = torch.cat((y[:i], y[i+1:]), dim=0)
-        indices_ind = indices==lbl
-        dist_idx = soft_dist[i]
-        print(y)
-        print(i, y[i])
-        print(indices)
-        print(dist_idx)
-        print(indices_ind)
-        ps = dist_idx[indices_ind]
-        print(ps)
-        print(torch.sum(ps))
-        print("=="*50)
+    # n = num_label[0]
+    # dist = dist.flatten()[1:].view(n-1, n+1)[:,:-1].reshape(n, n-1)
+    # print(dist)
+    # soft_dist = torch.softmax(-dist, dim=1)
+    # print(soft_dist)
+    # for i in range(n):
+    #     lbl = y[i]
+    #     indices = torch.cat((y[:i], y[i+1:]), dim=0)
+    #     indices_ind = indices==lbl
+    #     dist_idx = soft_dist[i]
+    #     print(y)
+    #     print(i, y[i])
+    #     print(indices)
+    #     print(dist_idx)
+    #     print(indices_ind)
+    #     ps = dist_idx[indices_ind]
+    #     print(ps)
+    #     print(torch.sum(ps))
+    #     print("=="*50)
 
-    
+    # endregion
+
+    dataset = Noiseprint_Dataset(paths=paths)
+    model = Noise_Print(input_shape=[1, 3, 48, 48], num_layers=17)
+
+    model.train()
+
+    X, y = dataset[0]
+    out = model(X)
+
+    print(out.shape)
 
 
 
