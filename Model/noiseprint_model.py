b68aefa75665698682b2e372265043dd1f368798
hamzeasadi <hamzeasadi1@gmail.com>
2023-12-11 09:13:38 +0000

    add all

--- Model/noiseprint_model.py
+++ Model/noiseprint_model.py
@@ -26,14 +26,14 @@ class Noise_Print(nn.Module):
         self.num_layers = num_layers
 
         self.blk0 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape[1], out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU()
+            nn.Conv2d(in_channels=input_shape[1], out_channels=64, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(inplace=True)
         )
 
         self.mid_blk = self._mid_blk()
 
         self.head = nn.Sequential(
-                nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1)
+                nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1, bias=True)
                 # nn.Sigmoid()
         )
 
@@ -42,8 +42,8 @@ class Noise_Print(nn.Module):
         layers = []
         for i in range(self.num_layers):
             layer = nn.Sequential(
-                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
-                nn.BatchNorm2d(num_features=64), nn.ReLU()
+                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True),
+                nn.BatchNorm2d(num_features=64, momentum=0.9, affine=True, eps=1e-4), nn.ReLU(inplace=True)
             )
             layers.append(layer)
         
@@ -70,7 +70,7 @@ def main():
 
     x = torch.randn(size=(1, 3, 64, 64))
 
-    model = Noise_Print(input_shape=[1,3,64,64], num_layers=17)
+    model = Noise_Print(input_shape=[1,3,64,64], num_layers=15)
 
     out = model(x)
     print(model)
