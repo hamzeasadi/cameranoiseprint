b0f6bc5480e32c62cd45d4fa17e47a89b9a9c3ee
hamzeasadi <hamzeasadi1@gmail.com>
2023-12-13 09:07:57 +0000

    all good

--- DataPrepare/utils.py
+++ DataPrepare/utils.py
@@ -51,10 +51,68 @@ def cvt2Intensity(img:Image):
 
 
 
-def get_cam_samples(cam_path, sample_per_patch:int):
+def get_cam_samples(cam_path, num_samples:int):
     """docs"""
     cam_videos = os.listdir(cam_path)
+    All_Triplets = []
+    sample_info = dict()
+    for video_name in cam_videos:
+        video_path = os.path.join(cam_path, video_name)
+        video_frames = sorted([f for f in os.listdir(video_path) if f.endswith(".png")])
+        num_video_frames = len(video_frames)
+        for i in range(0, num_video_frames-3, 3):
+            frame0_path = os.path.join(video_path, video_frames[i])
+            frame1_path = os.path.join(video_path, video_frames[i+1])
+            frame2_path = os.path.join(video_path, video_frames[i+2])
+            sample = [frame0_path, frame1_path, frame2_path]
+            All_Triplets.append(sample)
     
+    print(f"{cam_path}: {len(All_Triplets)}")
+    random.shuffle(All_Triplets)
+    sub_samples = random.sample(All_Triplets, num_samples)
+    subsize = len(sub_samples)
+    for i in range(0, subsize, 4):
+        sub0 = sub_samples[i]
+        sub1 = sub_samples[i+1]
+        sub2 = sub_samples[i+2]
+        sub3 = sub_samples[i+3]
+        sample_info[i//4] = (sub0, sub1, sub2, sub3)
+    
+    return sample_info
+
+
+
+
+def extract_cam_samples(cam_path:str, cam_id:int, crop_size:List):
+    """docs"""
+    
+
+
+
+
+
+
+def patchify(img_path, crop_size:List):
+    img = Image.open(img_path)
+    w, h = img.size
+    img_y = cvt2Intensity(img=img)
+    plt.imshow(img_y, cmap='gray')
+    plt.axis("off")
+    # hc, wc = crop_size
+    # num_h = h//hc
+    # num_w = w//wc
+    # fig, axs = plt.subplots(nrows=num_w, ncols=num_h)
+    # for i in range(num_h):
+    #     hi = i*hc
+    #     for j in range(num_w):
+    #         wi = j*wc
+    #         crop = img_y[hi:hi+hc, wi:wi+wc]
+    #         axs[j, i].imshow(crop, cmap='gray')
+    #         axs[j, i].axis("off")
+    
+   
+    plt.savefig("patchify.png", bbox_inches='tight', pad_inches=0, transparent = True)
+    plt.close()
 
 
 
@@ -64,8 +122,11 @@ def main():
     """
     root_path = "/mnt/exthd/Dataset/frames"
     paths = Paths()
-    
-    unfiy_frame_size(root_path=root_path)
+    img_base = "/mnt/exthd/Dataset/frames/100/Eurecom_100_video_001.mp4"
+    img_path = os.path.join(img_base, f"img_{1:08d}.png")
+    patchify(img_path=img_path, crop_size=[64, 64])
+    # get_cam_samples(cam_path=os.path.join(root_path, f"{100}"), num_samples=100)
+
     
 
 
