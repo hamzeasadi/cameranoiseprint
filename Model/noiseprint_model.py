40491f3fed552daafb35cab0a311c9d5df5d35e6
hamzeasadi <hamzeasadi1@gmail.com>
2023-12-14 00:11:34 +0000

    all good

--- Model/noiseprint_model.py
+++ Model/noiseprint_model.py
@@ -9,10 +9,48 @@ import torch
 from torch import nn
 
 
-class Const_layer(nn.Module):
+class ConstConv(nn.Module):
     """
-    docs
+    const conv
     """
+    def __init__(self, inch:int, outch:int, ks:int, stride:int, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.ks = ks
+        self.inch = inch
+        self.conv0 = nn.Conv2d(in_channels=inch, out_channels=outch, kernel_size=ks, stride=stride, padding=0, bias=False)
+        self.padding = nn.ZeroPad2d(1)
+        self.bn = nn.BatchNorm2d(num_features=outch)
+        self.act = nn.ReLU()
+        self.data = self._get_data()
+
+    def forward(self, x):
+        if self.training:
+            out0 = self.conv0(self.data['zero'])
+            out1 = self.conv0(self.data['one'])
+            out = self.padding(x)
+            out = self.conv0(out)
+            out = self.act(out)
+            return dict(out=out, out1=out1, out0=out0)
+        
+        x = self.padding(x)
+        x = self.conv0(x)
+        x = self.act(x)
+        return dict(out=x)
+
+    
+
+    def _get_data(self):
+        ones = torch.ones(size=(self.inch, self.ks, self.ks), dtype=torch.float32)
+        ones[:, self.ks//2, self.ks//2] = 0.0
+        zeros = torch.zeros_like(ones)
+        zeros[:, self.ks//2, self.ks//2] = 1.0
+
+        return dict(one=ones.unsqueeze(dim=0), zero=zeros.unsqueeze(dim=0))
+
+    
+
+
+
 
 
 
@@ -62,6 +100,59 @@ class Noise_Print(nn.Module):
 
 
 
+class Noise_PrintConst(nn.Module):
+    """
+    noiseprint module
+    """
+
+    def __init__(self, input_shape:List, num_layers:int, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.num_layers = num_layers
+
+        # self.blk0 = nn.Sequential(
+        #     nn.Conv2d(in_channels=input_shape[1], out_channels=64, kernel_size=3, stride=1, padding=1, bias=True),
+        #     nn.ReLU(inplace=True)
+        # )
+
+        self.blk0 = ConstConv(inch=3, outch=64, ks=3, stride=1)
+
+        self.mid_blk = self._mid_blk()
+
+        self.head = nn.Sequential(
+                nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1, bias=True),
+                nn.ReLU6(inplace=True)
+        )
+
+
+    def _mid_blk(self):
+        layers = []
+        for i in range(self.num_layers):
+            layer = nn.Sequential(
+                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True),
+                nn.BatchNorm2d(num_features=64, momentum=0.9, affine=True, eps=1e-4), nn.ReLU(inplace=True)
+            )
+            layers.append(layer)
+        
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.training:
+            out_dict = self.blk0(x)
+            x = self.mid_blk(out_dict['out'])
+            x = self.mid_blk(x)
+            out = self.head(x)
+            return dict(out=out, out1=out_dict['out1'], out0=out_dict['out0'])
+        else:
+            out_dict = self.blk0(x)
+            x = self.mid_blk(out_dict['out'])
+            x = self.mid_blk(x)
+            out = self.head(x)
+            return dict(out=out)
+
+
+
+
+
 
 def main():
     """
@@ -72,12 +163,11 @@ def main():
 
     model = Noise_Print(input_shape=[1,3,64,64], num_layers=15)
 
-    out = model(x)
-    print(model)
+    cont_layer = ConstConv(inch=3, outch=3, ks=3, stride=1)
+
+    cont_layer.eval()
+    out = cont_layer(x)
     print(out)
-    print(out.shape)
-
-
 
 
 
--- main.py
+++ main.py
@@ -26,45 +26,7 @@ def main():
     """
 
     paths = Paths()
-    # region
-
-    # x = torch.tensor([
-    #                 [1, 2, 3],
-    #                 [2, 1, 3],
-    #                 [2, 2, 2],
-    #                 [4, 5, 6],
-    #                 [4, 5, 5],
-    #                 [1, 3, 3]
-    #             ], dtype=torch.float32)
-    # y = torch.tensor([1,0,1,0,1,0], dtype=torch.long)
-    # num_label = y.squeeze().size()
-    # print(x.shape)
-    # print(y.shape)
-
-
-    # dist = torch.cdist(x1=x, x2=x, p=2)
-    
-    # n = num_label[0]
-    # dist = dist.flatten()[1:].view(n-1, n+1)[:,:-1].reshape(n, n-1)
-    # print(dist)
-    # soft_dist = torch.softmax(-dist, dim=1)
-    # print(soft_dist)
-    # for i in range(n):
-    #     lbl = y[i]
-    #     indices = torch.cat((y[:i], y[i+1:]), dim=0)
-    #     indices_ind = indices==lbl
-    #     dist_idx = soft_dist[i]
-    #     print(y)
-    #     print(i, y[i])
-    #     print(indices)
-    #     print(dist_idx)
-    #     print(indices_ind)
-    #     ps = dist_idx[indices_ind]
-    #     print(ps)
-    #     print(torch.sum(ps))
-    #     print("=="*50)
-
-    # endregion
+  
 
     parser = argparse.ArgumentParser(prog=os.path.basename(__file__), description="config for training")
     parser.add_argument("--lr", type=float, required=True, default=0.01)
@@ -95,15 +57,22 @@ def main():
     opt = Adam(params=model.parameters(), lr=args.lr, weight_decay=0.0001)
     # opt = SGD(params=model.parameters(), lr=args.lr, momentum=0.9, weight_decay=0.0001)
     scheduler = ExponentialLR(opt, gamma=args.gamma)
-    
-    
+    crt_const = nn.BCEWithLogitsLoss()
+    crt_sum = nn.MSELoss()
     for epoch in range(args.epochs):
         model.train()
         train_loss = 0.0
         for i in range(dataset_size):
             X, y = dataset[i]
             out = model(X.to(dev))
-            loss = criterion(out, y.to(dev), psd_flag=args.psd, epoch=1)
+            loss1 = criterion(out['out'], y.to(dev), psd_flag=args.psd, epoch=1)
+
+            y_pos = torch.zeros_like(out['out0'], requires_grad=False)
+            const_loss = crt_sum(out['out0']+out['out1'], y_pos)
+            this_label = torch.ones_like(out['out0'], requires_grad=False)
+            another_loss = crt_const(0.1*out['ou0'], this_label)
+            loss = loss1 + another_loss + const_loss
+
             opt.zero_grad()
             loss.backward()
             opt.step()
@@ -113,7 +82,7 @@ def main():
         print(f"epoch={epoch} loss={train_loss:1.4f}")
         info = dict(model=model.eval().state_dict(), loss=train_loss/dataset_size)
         
-        torch.save(obj=info, f=os.path.join(paths.model, f"ckpoint_{epoch}.pt"))
+        torch.save(obj=info, f=os.path.join(paths.model, f"ckpointconst_{epoch}.pt"))
 
 
 
